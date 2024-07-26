ebb7cc6efc31ec044fb2af12d909db2598719769
Alex Bratov <104260167+THEMETALLEGEND@users.noreply.github.com>
2024-07-26 13:41:46 +0500

    Spawning and despawning functional
    Hiccups were indeed because of the shitty laptop.

--- Assets/Scripts/Fish/Spawner.cs
+++ Assets/Scripts/Fish/Spawner.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-	public string fishTag; // Tag to identify which fish to spawn
-	private GameObject spawnedFish = null;
+	public GameObject fish;
 	public bool hasSpawned = false;
-	private ObjectPool objectPool;
+	[HideInInspector] public GameObject spawnedFish;
 
+	// Start is called before the first frame update
 	void Start()
 	{
 		GameObject model = transform.GetChild(0).gameObject;
 		model.SetActive(false);
-		objectPool = FindObjectOfType<ObjectPool>(); // Find the ObjectPool in the scene
 	}
 
 	public void SpawnFish()
 	{
-		if (objectPool != null)
-		{
-			GameObject model = transform.GetChild(0).gameObject;
-			model.SetActive(false);
-			spawnedFish = objectPool.GetObject(fishTag);
-			spawnedFish.transform.position = transform.position;
-			hasSpawned = true;
-		}
+		GameObject model = transform.GetChild(0).gameObject;
+		model.SetActive(false);
+		spawnedFish = Instantiate(fish, transform.position, Quaternion.identity);
+		hasSpawned = true;
+	}
+
+	public void DespawnFish()
+	{
+		Destroy(spawnedFish);
+		hasSpawned = false;
 	}
 
 	private void OnTriggerEnter2D(Collider2D other)
@@ -35,18 +36,15 @@ public class Spawner : MonoBehaviour
 		{
 			SpawnFish();
 		}
+		//else
+		//Debug.Log("collided with something else");
 	}
 
 	private void OnTriggerExit2D(Collider2D other)
 	{
-		Debug.Log("Stopped colliding with " + other.gameObject);
 		if (other.tag == "Spawner" && hasSpawned)
 		{
-			if (objectPool != null)
-			{
-				objectPool.ReturnObject(spawnedFish);
-				hasSpawned = false;
-			}
+			DespawnFish();
 		}
 	}
 }
